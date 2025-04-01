75aba43c1d877769e2452f65412d541be5fc30dd
Bhawana Rokey <Bhawanarokey@gmail.com>
2025-04-01 18:44:31 +0530

    Corrected some mistakes

--- Age_Calculator.py
+++ Age_Calculator.py
@@ -1,14 +1,13 @@
-def days_in_month(self,m,year):
-    if m==1:
-        m=12
-    if m==1 or m==3 or m==5 or m==7 or m==8 or m==10 or m==12:
+def days_in_month(m,y):
+    m1=int(m)-1
+    if m1==0:
+        m1=12
+    if m1==1 or m1==3 or m1==5 or m1==7 or m1==8 or m1==10 or m1==12:
         days=31
-    elif m==4 or m==6 or m==9 or m==11:
+    elif m1==4 or m1==6 or m1==9 or m1==11:
         days=30
-    elif m==2:
-        if year%400==0:
-            days=29
-        elif year%10!=0 and year%4==0:
+    elif m1==2:
+        if y%400==0 or (y%100!=0 and y%4==0):
             days=29
         else:
             days=28
